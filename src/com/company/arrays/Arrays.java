eecb7bc67f9536fed29d292b47201698b922070b
Shark <szczerbicki.pawel@gmail.com>
2017-11-01 13:23:26 +0100

    add comments

--- src/com/company/arrays/Arrays.java
+++ src/com/company/arrays/Arrays.java
@@ -3,7 +3,6 @@ package com.company.arrays;
 public class Arrays {
 
     public static void main(String[] args) {
-        System.out.println();
         int test1 = 2;
         int test2 = 2;
         int test3 = 2;
@@ -12,6 +11,8 @@ public class Arrays {
 
         String a = new String("test");
         String b = "test";
+
+        //init array withot values
         int[] intTab = new int[20];
         String[] intString = new String[20];
 
@@ -20,10 +21,15 @@ public class Arrays {
         intTab[5]=10;
         intTab[2]=20;
 
+        //print without new line. Prints reference to array
         System.out.print(intTab);
+
+        //print 5 element from array
         System.out.println(intTab[5]);
+
         System.out.println(intTab[19]);
 
+        //init array with values
         int[] tabWithElements = new int[]{1,10,34,321};
 
         int testIncrement = 0;
@@ -36,15 +42,6 @@ public class Arrays {
 
         //position 1
         System.out.println(tabWithElements[++testIncrement]);
-
-
-
-
-
-
-
-
-
     }
 
 }
